d3aec0eaf2494cee4c98e8a3aef46e1d2bfed140
adeankush3 <adeankush3@gmail.com>
2022-02-13 12:29:09 +0530

    [Ankush]Started A Data Stucture Core Programming Linked List

---
+++ DataStructureLinkedList/DataStructureLinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Data Dtucture In Linked List");
+        }
+    }
+}
