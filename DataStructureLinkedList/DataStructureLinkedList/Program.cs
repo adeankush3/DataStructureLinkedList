baa9a7461f87ce2e71942bb1d82ce97a36064eaf
adeankush3 <adeankush3@gmail.com>
2022-02-14 20:21:01 +0530

    [Ankush]Queue Operation Complete

--- DataStructureLinkedList/DataStructureLinkedList/Program.cs
+++ DataStructureLinkedList/DataStructureLinkedList/Program.cs
@@ -17,21 +17,15 @@ namespace DataStructureLinkedList
             list.Add(30);
 
             list.Display();
-
             list.InsertAtParticularPosition(2, 70);
-
             list.AddInReverseOrder(30);
-
             list.RemoveFirstNode();
-
             list.RemoveLastNode();
-
             list.search(2);
-
             list.DeleteNodeAtparticularPosition(2);
-
             list.size();
 
+           
             Stack stack = new Stack();
             stack.Push(70);
             stack.Push(30);
@@ -40,6 +34,14 @@ namespace DataStructureLinkedList
             stack.Peek();
             stack.Pop();
             stack.IsEmpty();
+
+           
+            Queue queue=new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Dequeue();
+            queue.Display();
 
         }
     }
