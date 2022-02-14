8c23a253702ac5d674e1d954ff33d05cda974e0b
adeankush3 <adeankush3@gmail.com>
2022-02-14 20:13:35 +0530

    [Ankush]Stack operation complete

--- DataStructureLinkedList/DataStructureLinkedList/Program.cs
+++ DataStructureLinkedList/DataStructureLinkedList/Program.cs
@@ -31,6 +31,16 @@ namespace DataStructureLinkedList
             list.DeleteNodeAtparticularPosition(2);
 
             list.size();
+
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.IsEmpty();
+
         }
     }
 }
